e40d46262ef73cbde172cc51ec3f360ddde7bd9f
Maxim Kapitonov <kapitonovmaxim@gmail.com>
2025-05-20 01:09:18 +0300

    save 03

---
+++ 03.lesson3-props/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Каталог товаров</h1>
+    <div class="products">
+      <product-card v-for="product in products" :key="product.id" :product="product" />
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import products from "./assets/products.json";
+import './assets/products.sass'
+import ProductCard from './components/ProductCard.vue';
+
+
+onMounted (() => {
+console.log(products);
+});
+</script>
+
+<style scoped>
+@import 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css';
+
+</style>
