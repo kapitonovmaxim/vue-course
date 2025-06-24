a32356354feccfb5953e1d7e95986c3a9ef0e506
Maxim Kapitonov <kapitonovmaxim@gmail.com>
2025-06-24 15:05:34 +0300

    del logs

--- 07.lesson-test/src/spec/CheckoutView.spec.js
+++ 07.lesson-test/src/spec/CheckoutView.spec.js
@@ -61,65 +61,7 @@ describe('CheckoutView Component', () => {
         expect(wrapper.find('.checkout-button').text()).toBe('Перейти на главную')
     })
 
-    // test('отображает товары и общую сумму, когда корзина не пуста', async () => {
-    //     // Настраиваем моки
-    //     store.items = [
-    //         {
-    //             id: 1,
-    //             title: 'Test Product 1',
-    //             price: 10,
-    //             images: ['image1.jpg'],
-    //             amount: 2
-    //         },
-    //         {
-    //             id: 2,
-    //             title: 'Test Product 2',
-    //             price: 20,
-    //             images: ['image2.jpg'],
-    //             amount: 1
-    //         }
-    //     ]
-
-    //     store.getCartProduct.mockImplementation((id) => {
-    //         if (id === 1) return { amount: 2 }
-    //         if (id === 2) return { amount: 1 }
-    //         return null
-    //     })
-
-    //     store.total = 40 // 10*2 + 20*1
-
-    //     wrapper = mount(CheckoutView, {
-    //         global: {
-    //             plugins: [router],
-    //         },
-    //     })
-
-    //     await flushPromises()
-
-    //     console.log('wrapper', wrapper.html());
-
-    //     // Проверяем отображение товаров
-    //     const productItems = wrapper.findAll('.product-item')
-    //     expect(productItems.length).toBe(2)
-
-    //     // Проверяем отображение названий товаров
-    //     expect(wrapper.text()).toContain('Test Product 1')
-    //     expect(wrapper.text()).toContain('Test Product 2')
-
-    //     // Проверяем отображение цен и количества
-    //     const prices = wrapper.findAll('.product-price')
-    //     expect(prices[0].text()).toBe('10$')
-    //     expect(prices[1].text()).toBe('20$')
-
-    //     const quantities = wrapper.findAll('.product-quantity')
-    //     expect(quantities[0].text()).toBe('× 2')
-    //     expect(quantities[1].text()).toBe('× 1')
-
-    //     // Проверяем расчет общей суммы
-    //     expect(wrapper.find('.total-price').text()).toBe('Общая сумма: 40$')
-    // })
-
-        test('корректно отображает товары в корзине', async () => {
+    test('корректно отображает товары в корзине', async () => {
         // Настраиваем моки
         store.items = [
             {
